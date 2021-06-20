352c6ba11b975a8a492c2b1950f0ae598fb5b680
devildev98 <debarghya.datta89@gmail.com>
2021-06-20 21:04:44 +0530

    Fish Config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,43 +1,92 @@
 # ~/.config/fish/config.fish
 #
-#    >=>                            >=>     >=>                       
-#    >=>                        >>  >=>     >=>                       
-#    >=>   >==>    >=>     >=>      >=>     >=>   >==>    >=>     >=> 
-#  >=>>=> >>   >=>   >=>   >=>  >=>  >=>  >=>>=> >>   >=>   >=>   >=>  
-# >>  >=> >>===>>=>   >=> >=>   >=>  >=> >>  >=> >>===>>=>   >=> >=>   
-# >>  >=> >>           >=>=>    >=>  >=> >>  >=> >>           >=>=>    
-#  >=>>=>  >====>       >=>     >=> >==>  >=>>=>  >====>       >=>     
+#    >=>                            >=>     >=>
+#    >=>                        >>  >=>     >=>
+#    >=>   >==>    >=>     >=>      >=>     >=>   >==>    >=>     >=>
+#  >=>>=> >>   >=>   >=>   >=>  >=>  >=>  >=>>=> >>   >=>   >=>   >=>
+# >>  >=> >>===>>=>   >=> >=>   >=>  >=> >>  >=> >>===>>=>   >=> >=>
+# >>  >=> >>           >=>=>    >=>  >=> >>  >=> >>           >=>=>
+#  >=>>=>  >====>       >=>     >=> >==>  >=>>=>  >====>       >=>
 #
 
 
-function bind_bang
-    switch (commandline --current-token)[-1]
-    case "!"
-        # Without the `--`, the functionality can break when completing
-        # flags used in the history (since, in certain edge cases
-        # `commandline` will assume that *it* should try to interpret
-        # the flag)
-        commandline --current-token -- $history[1]
-        commandline --function repaint
-    case "*"
-        commandline --insert !
+# Hide welcome message
+set fish_greeting
+set VIRTUAL_ENV_DISABLE_PROMPT "1"
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+set EDITOR "nvim"
+
+## Environment setup
+# Apply .profile
+source ~/.profile
+
+# Add ~/.local/bin to PATH
+if test -d ~/.local/bin
+    if not contains -- ~/.local/bin $PATH
+        set -p PATH ~/.local/bin
     end
+end
+
+# Replace cat with bat
+abbr cat='bat --style header --style rules --style snip --style changes --style header'
+
+# yay and paru
+[ ! -x /usr/bin/yay ] && [ -x /usr/bin/paru ] && alias yay='paru'
+
+
+## Functions
+# Functions needed for !! and !$ https://github.com/oh-my-fish/plugin-bang-bang
+function __history_previous_command
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function __history_previous_command_arguments
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
 end
 
 function fish_user_key_bindings
     bind ! bind_bang
 end
 
-### SETTING THE STARSHIP PROMPT ###
-starship init fish | source
+
+## Starship prompt
+if status --is-interactive
+   source ("/usr/bin/starship" init fish --print-full-init | psub)
+end
 
 ### pfetch ###
-pfetch | lolcat
-# alacritty-pywal &
+if status --is-interactive
+   pfetch|lolcat
+end
 
-### EXPORT ###
-set fish_greeting                      # Supresses fish's intro message
 
+if [ "$fish_key_bindings" = fish_vi_key_bindings ];
+  bind -Minsert ! __history_previous_command
+  bind -Minsert '$' __history_previous_command_arguments
+else
+  bind ! __history_previous_command
+  bind '$' __history_previous_command_arguments
+end
+
+# Fish command history
+function history
+    builtin history --show-time='%F %T '
+end
+
+function backup --argument filename
+    cp $filename $filename.bak
+end
 
 ### RANDOM COLOR SCRIPT ###
 #colorscript random
@@ -49,14 +98,13 @@ set fish_color_command brcyan
 set fish_color_error '#ff6c6b'
 set fish_color_param brcyan
 
-### DEv ###
-#bind \cl 'echo -n (clear | string replace \e[3J] ""); commandline -f repaint'
 
 # Changing "ls" to "exa"
-alias ls='exa -al --color=always --group-directories-first' # my preferred listing
-alias la='exa -a --color=always --group-directories-first'  # all files and dirs
-alias ll='exa -l --color=always --group-directories-first'  # long format
-alias lt='exa -aT --color=always --group-directories-first' # tree listing
+abbr ls='exa -al --color=always --group-directories-first --icons' # my preferred listing
+abbr la='exa -a --color=always --group-directories-first --icons'  # all files and dirs
+abbr ll='exa -l --color=always --group-directories-first --icons'  # long format
+abbr lt='exa -aT --color=always --group-directories-first --icons' # tree listing
+abbr l.="exa -a | egrep '^\.'"                                     # show only dotfiles
 
 # Directory Traversal
 alias ..= 'cd ..'
@@ -66,29 +114,32 @@ alias .4='cd ../../../..'
 alias .5='cd ../../../../..'
 
 # Colorize grep output (good for log files)
-alias grep='grep --color=auto'
-alias egrep='egrep --color=auto'
-alias fgrep='fgrep --color=auto'
-alias v='nvim'
+abbr grep='grep --color=auto'
+abbr egrep='egrep --color=auto'
+abbr fgrep='fgrep --color=auto'
+abbr v='nvim'
 
 # confirm before overwriting something
-alias cp="cp -i"
-alias mv='mv -i'
-alias rm='rm -i'
+abbr cp="cp -i"
+abbr mv='mv -i'
+abbr rm='rm -i'
 
 # adding flags
-alias df='df -h'                          # human-readable sizes
-alias free='free -m'                      # show sizes in MB
-# Python alias
-alias py='python3'
+abbr df='df -h'                          # human-readable sizes
+abbr free='free -m'                      # show sizes in MB
+# Python abbr
+abbr py='python3'
 
 # config files
-alias fishconf='nvim ~/.config/fish/config.fish'
-alias kittyconf='nvim ~/.config/kitty/kitty.conf'
-alias vimconf='nvim ~/.config/nvim/init.vim'
+abbr fishconf='nvim ~/.config/fish/config.fish'
+abbr kittyconf='nvim ~/.config/kitty/kitty.conf'
+abbr vimconf='nvim ~/.config/nvim/init.vim'
 
-# youtube-dl alias
-alias ytm='youtube-dl -x --audio-format mp3 --audio-quality 320k  -o "~/Music/%(title)s.%(ext)s"'
-alias ytmdl='ytmdl --ignore-errors'
-alias mpv='devour mpv'
-alias ytdl='youtube-dl'
+# youtube-dl abbr
+abbr ytm='youtube-dl -x --audio-format mp3 --audio-quality 320k  -o "~/Music/%(title)s.%(ext)s"'
+abbr ytmdl='ytmdl --ignore-errors'
+abbr mpv='devour mpv'
+abbr ytdl='youtube-dl'
+
+# Cleanup orphaned packages
+abbr cleanup='sudo pacman -Rns (pacman -Qtdq)'
